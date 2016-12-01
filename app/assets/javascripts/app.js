eddc7c1bd92490cfc760117166b7d3f983632985
Rachel Tublitz <tublitzed@gmail.com>
2016-11-30 22:42:14 -0500

    Wire front-end to change colors, adding sample aws config files

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,21 +1,43 @@
 var App = {
+
   /**
-   * Create a new pusher connection, and subscribe to
+   * Create a new Pusher connection, and subscribe to
    * a channel.
    */
-  initPusher: function () {
+  initPusher: function() {
+    var self = this;
     var appId = $('input[name="pusher-app-key"]').val();
     var pusher = new Pusher(appId, {
       encrypted: true
     });
 
     var channel = pusher.subscribe('alexa_channel');
-    channel.bind('incoming_event', function (data) {
-      console.log(data);
+    channel.bind('incoming_event', function(response) {
+      console.log(response);
+      var color = response.data.request.intent.name.toLowerCase();
+      self.changeColor(color);
     });
+  },
+
+  /**
+   * Change background color of element.
+   * @param {string} color
+   */
+  changeColor: function(color) {
+    this.$el.css({
+      background: color
+    });
+  },
+
+  /**
+   * Cache main element, and then setup Pusher.
+   */
+  init: function() {
+    this.$el = $('.demo');
+    this.initPusher();
   }
 };
 
-$(function () {
-  App.initPusher();
+$(function() {
+  App.init();
 });
